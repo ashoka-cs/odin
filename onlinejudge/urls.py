81e42ccce84e676ee7f65d41582982c962dbf1d9
Barun Parruck <barun.parruck@gmail.com>
2017-10-10 21:06:31 +0530

    Created basic login functionality

--- onlinejudge/urls.py
+++ onlinejudge/urls.py
@@ -1,11 +1,12 @@
 from . import views
 from django.conf.urls import url
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-   url(r'^$', views.index, name="index"),
-   url(r'^signup/$', views.signup, name="signup"),
-   url(r'^login/$', views.login, name="login"),
-   url(r'^problemset/$', views.problemset, name="problemset")
+    #url(r'^$', views.index, name="index"),
+    url(r'^signup/$', views.signup, name="signup"),
+    url(r'', auth_views.login, name="login"),
+    #url(r'^login/$', views.login, name="login"),
+    url(r'^problemset/$', views.problemset, name="problemset")
    
 ]
