c3c03b31d8f7db95d01206a6eda5c814b9c67663
paulkurian <paul.kurian_ug19@ashoka.edu.in>
2017-11-07 00:48:36 +0530

    1)Defined model SubmissionForm. 2)Use model SubmissionnForm instead of the form SubmissionForm in forms.py 3)Updated views.py in order save the user name of the user that is currently logged in as opposed to giving the user the option to select username from the User database

--- onlinejudge/models.py
+++ onlinejudge/models.py
@@ -14,19 +14,9 @@ class Problem(models.Model):
         return str(self.problem_id) + " : " + self.problem_statement[0:10]
 
 class Submission(models.Model):
-<<<<<<< HEAD
     problem = models.ForeignKey(Problem, on_delete= models.CASCADE)
     time_of_submission = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-=======
-    problem_id = models.ForeignKey(Problem)
-    time__of_submission = datetime.datetime.now()
-    user_id = models.ForeignKey(User)
-
-    LANGUAGES = [['py','Python 3'],['cpp','C++']]
-
-    language = models.CharField(max_length = 20, default = 'py', choices = LANGUAGES)
->>>>>>> 39f1c32d1097ae062094e9dd64cea846bc384638
     code = models.TextField()
     LANGUAGES = [['py','Python 3'],['cpp','C++']]
     language = models.CharField(max_length = 20, default = 'py', choices = LANGUAGES)
