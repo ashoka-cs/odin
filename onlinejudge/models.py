d5601a080e604b72cef7288dad246bb8894a7d1f
aniruddha-jafa <jafa.aniruddha@gmail.com>
2017-11-06 21:03:41 +0530

    Added model for submissions, registered with the admin, and made the appropriate migrations to the database.

--- onlinejudge/models.py
+++ onlinejudge/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+import datetime
+
 
 # Create your models here.
 class Problem(models.Model):
     problem_id = models.CharField(max_length=30, primary_key=True)
     problem_title = models.CharField(max_length=30)
     problem_statement = models.TextField()
+
     def __str__(self):
         return str(self.problem_id) + " : " + self.problem_statement[0:10]
+
+class Submission(models.Model):
+    problem_id = models.ForeignKey(Problem)
+    time__of_submission = datetime.datetime.now()
+    user_id = models.ForeignKey(User)
+    code = models.TextField()
