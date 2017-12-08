2d0c46004cb3c5a87c20c8aea11bb28c8996ca35
Barun Parruck <barun.parruck@gmail.com>
2017-12-08 23:30:04 +0530

    Fixed merges

--- onlinejudge/views.py
+++ onlinejudge/views.py
@@ -13,13 +13,6 @@ from django.contrib.auth.forms import UserCreationForm
 def index(requests):
     return render(requests, 'index.html', {}) # the third argument is for variables to be passed to the file
 
-<<<<<<< HEAD
-=======
-#
-def signup(requests): 
-    return render(requests, 'signup.html', {})
-
->>>>>>> 060237c1e244f6fe23053d37fc9515b913d00d9c
 # redundant view
 def login(requests):
     return render(requests, 'login.html', {})
@@ -54,11 +47,7 @@ def submissions(requests):
 
 def signup(requests):
     if requests.method == 'POST':
-<<<<<<< HEAD
         form = UserCreationForm(requests.POST)
-=======
-        form = UserCreationForm(request.POST)
->>>>>>> 060237c1e244f6fe23053d37fc9515b913d00d9c
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -77,10 +66,6 @@ def contestlist(requests):
 
 def contest_detail(requests, contest_pk):
     contest = get_object_or_404(Contest, pk = contest_pk)
-<<<<<<< HEAD
-    problems=Problem.objects.filter(contest=str(contest_pk))
-    return render(requests, 'contest_detail.html', {'contest': contest,'problems': problems})
-=======
         
     start_time=contest.start_time
     end_time=contest.end_time
@@ -102,5 +87,3 @@ def problem_detail(requests, problem_pk):
         return submissions(requests, problem) #render(requests, 'problem_detail.html', {'problem': problem, 'form': form})
     elif requests.method == 'POST':
         return submissions(requests)
-
->>>>>>> 060237c1e244f6fe23053d37fc9515b913d00d9c
